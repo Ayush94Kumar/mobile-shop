15ca3422ad196287971386a138ed2a7bfd91d54d
Ayush kumar <ayush76671kumar@gmail.com>
2025-12-28 22:50:56 +0530

    change_background&add_more_product

--- script.js
+++ script.js
@@ -35,7 +35,15 @@ document.addEventListener("DOMContentLoaded", () => {
     { name: "Realme C55", price: 11999, image: "image/realmec55.jpg" },
 
     { name: "Poco X5 Pro", price: 22999, image: "image/poco_x5pro.jpg" },
-    { name: "Poco M6", price: 10999, image: "image/poco_m6.jpg" }
+    { name: "Poco M6", price: 10999, image: "image/poco_m6.jpg" },
+    //electronic appliances
+
+    { name: "Voltas 1.5 Ton AC", price: 32999, image: "image/ac_voltas.jpg", category: "electronics" },
+    { name: "LG Frost Free Refrigerator", price: 28999, image: "image/fridge_lg.jpg", category: "electronics" },
+    { name: "Whirlpool Deep Freezer 250L", price: 19999, image: "image/freezer_whirlpool.jpg", category: "electronics" },
+    { name: "Samsung 43\" Smart TV", price: 24999, image: "image/tv_samsung43.jpg", category: "electronics" },
+    { name: "IFB 6kg Washing Machine", price: 14999, image: "image/wash_ifb.jpg", category: "electronics" },
+    { name: "Samsung Microwave 25L", price: 6999, image: "image/microwave_samsung.jpg", category: "electronics" }
   ];
 
   /* ================= RENDER PRODUCTS ================= */
